91a736cbefb9e30b51e581b520f1cd9d08ba7b96
Cenyol <cenyol@163.com>
2017-11-12 19:21:46 +0800

    -

--- src/main/java/com/cenyol/study/drools/RuleRunner.java
+++ src/main/java/com/cenyol/study/drools/RuleRunner.java
@@ -16,41 +16,19 @@
 
 package com.cenyol.study.drools;
 
-import com.cenyol.study.callback.SensorDataCbk;
-import org.drools.core.impl.InternalKnowledgeBase;
-import org.drools.core.impl.KnowledgeBaseFactory;
-import org.kie.api.definition.KiePackage;
-import org.kie.api.io.ResourceType;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.internal.builder.KnowledgeBuilder;
-import org.kie.internal.builder.KnowledgeBuilderFactory;
-import org.kie.internal.io.ResourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Collection;
 
 public class RuleRunner {
     Logger logger = LoggerFactory.getLogger(RuleRunner.class);
 
-    private String ruleBaseDirectory = "rules/";
-
-    public RuleRunner() {
-    }
-
-    public void runRules(String[] rules,
-                         Object[] facts) {
-
-        InternalKnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
-        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-        for ( int i = 0; i < rules.length; i++ ) {
-            String ruleFile = rules[i];
-            kbuilder.add( ResourceFactory.newClassPathResource( ruleBaseDirectory + ruleFile ),
-                                  ResourceType.DRL );
-        }
-        Collection<KiePackage> pkgs = kbuilder.getKnowledgePackages();
-        kbase.addPackages( pkgs );
-        KieSession ksession = kbase.newKieSession();
+    public void runRules(String[] rules, Object[] facts) {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+        KieSession ksession = kc.newKieSession("airData-rulesKS");
 
         for ( int i = 0; i < facts.length; i++ ) {
             Object fact = facts[i];
